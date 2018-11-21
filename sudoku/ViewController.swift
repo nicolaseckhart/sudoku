0edb986a620de381ce3fa4fd29b71868478423c2
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2018-11-21 14:30:27 +0100

    Initial Commit

---
+++ sudoku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sudoku
+//
+//  Created by Nicolas Eckhart on 21.11.18.
+//  Copyright © 2018 Nicolas Eckhart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
